af0beca3269458c544a0e8eadf73d27603dfe4d0
MWernerTech <mwerner.dalhousie@gmail.com>
2012-06-15 18:25:53 +0200

    added files

---
+++ TabletCalc/src/com/mwerner/calc4tablet/Fragment_FVAnn_Info.java
@@ -0,0 +1,16 @@
+package com.mwerner.calc4tablet;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_FVAnn_Info extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.fragment_pvann_info, container, false);
+	}
+}
